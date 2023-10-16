16ce99f5207cbc182dfbf7b91074a0093a498ba5
Devin <demolnau@iastate.edu>
2023-10-16 15:20:50 -0500

    react router practice

---
+++ first_router_react/src/main.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Router from "./Router";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import App from "./App";
+// import Profile from "./Profile";
+// import DefaultProfile from "./DefaultProfile";
+// import Spinach from "./Spinach";
+// import Popeye from "./Popeye";
+
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//   },
+//   {
+//     path: "profile",
+//     element: <Profile />,
+//     // children: [
+//     //   { index: true, element: <DefaultProfile /> },
+//     //   { path: "spinach", element: <Spinach /> },
+//     //   { path: "popeye", element: <Popeye /> },
+//     // ],
+//   },
+// ]);
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+     <Router />
+  </React.StrictMode>
+);
